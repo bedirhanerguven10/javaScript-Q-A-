577bdd72377f9a9791f4698e31edd25055fb3736
Bedirhan <bedirhan.erguven@outlook.com>
2022-05-14 23:41:25 +0300

    js_exercises

---
+++ length_of_last_word.js
@@ -0,0 +1,12 @@
+// Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+// let sentence= "   fly me   to   the moon  ";
+// let last=sentence.trim();
+// console.log(last);
+// let arr=last.split(" ");
+// console.log(last.split(" "));
+// console.log(arr[arr.length-1].length);
+
+
