d9bcefaa0205aa939a641d88c84f0a1d8092ed89
Bedirhan <bedirhan.erguven@outlook.com>
2022-05-17 17:18:12 +0300

    team_js

---
+++ array_filter.js
@@ -0,0 +1,22 @@
+// 1-JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+
+// Solution 1
+// let numbers = [2, 4, 5, 6];
+
+// let pedro = numbers.filter(evenNumber);
+
+// function evenNumber(num) {
+//     if (num % 2 == 0) {
+//     return num
+// }}
+// console.log(pedro);
+
+// Solution 2
+
+// let navas = numbers.filter((t) => !(t%2));
+// console.log(navas);
