5e65cface3983310aaaebf8d71316a81b05156d9
Bedirhan <bedirhan.erguven@outlook.com>
2022-05-26 23:33:57 +0300

    qs

---
+++ count-duplicatenumber.js
@@ -0,0 +1,13 @@
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// let taka = "cekoslovakyalilastiramadiKLArimizden misiniz 123*0**56775".toLowerCase();
+// let tiki= {};
+// for (let i = 0; i< taka.length ; i++){
+//     if (taka[i] in tiki){
+//         tiki[taka[i]] += 1;
+//     } else{
+//         tiki[taka[i]] = 1;
+//     }
+// }
+// console.log(tiki);
