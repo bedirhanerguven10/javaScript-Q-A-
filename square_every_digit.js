8c452db2c9f381ea6dbc99442a6a0c58e71b4515
Bedirhan <bedirhan.erguven@outlook.com>
2022-05-26 23:27:05 +0300

    new-qs

---
+++ square_every_digit.js
@@ -0,0 +1,15 @@
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+// let pedro = x.split("").map((g) => g**2).join("");
+// console.log(pedro);
+
+// let x = "9119";
+// let carl = x.split("").map(kareal).join("")
+// function kareal(g){
+//     return g ** 2;
+// }
+// console.log(carl);
